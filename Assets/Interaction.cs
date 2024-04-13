8ab4d5a2bacd1186aa5e0c518a8a8e38bcea21ed
NycoBartels <75326303+NycoBartels@users.noreply.github.com>
2024-04-13 16:00:50 +0200

    fixed interaction and ragdoll

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Interaction : MonoBehaviour {
 
     private GameObject _cam;
-    private Transform holdingObj;
+    private Rigidbody holdingObj;
     private Transform handRoot;
-    private float playerInteractDistance = 1.5f;
+    [SerializeField] private float playerInteractDistance = 3f;
     private bool isActive = false;
+
+    public Transform grabObj;
+    private float previousDrag;
+    private float previousMass;
+    [SerializeField] private float holdingMassModifier = 100;
 
     private void Awake() {
         _cam = GameObject.FindGameObjectWithTag("MainCamera");
         handRoot = transform.GetChild(2);
+        Physics.IgnoreLayerCollision(3, 3);
     }
 
     private void Update() {
@@ -29,17 +35,24 @@ public class Interaction : MonoBehaviour {
         Interact();
     }
     private void DragObject() {
-        holdingObj.position = handRoot.position;
+        holdingObj.transform.position = handRoot.position;
     }
     private void DropObject() {
+        holdingObj.mass = previousMass;
+        holdingObj.drag = previousDrag;
         holdingObj = null;
     }
     private void Interact() {
+
+        
         RaycastHit hit;
         isActive = Physics.Raycast(_cam.transform.position, _cam.transform.TransformDirection(Vector3.forward), out hit, playerInteractDistance);
-
-        if (isActive && hit.transform.GetComponent<CharacterJoint>() != null) {
-            holdingObj = hit.transform;
+        if (isActive && hit.transform.GetComponent<Rigidbody>() != null) {
+            holdingObj = hit.transform.GetComponent<Rigidbody>();
+            previousMass = holdingObj.mass;
+            previousDrag = holdingObj.drag;
+            holdingObj.mass = holdingMassModifier;
+            holdingObj.drag = holdingMassModifier;
         }
     }
 }
